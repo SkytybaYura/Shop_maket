511981e8a810a30a9f163156f760886fa2e75ce8
Yura <yura05061997@gmail.com>
2019-11-18 13:32:16 +0000

    form

--- js/form.js
+++ js/form.js
@@ -10,12 +10,12 @@ function login(e) {
             // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify({
-            name: document.querySelector('.login-form input[name=name]').value,
+            username: document.querySelector('.login-form input[name=username]').value,
             surname: document.querySelector('.login-form input[name=surname]').value,
             mail: document.querySelector('.login-form input[name=mail]').value,
             password: document.querySelector('.login-form input[name=pass]').value,
-            phone: document.querySelector('.login-form input[name=phone]').value,
-            gender: document.querySelector('.login-form input[name=gender]').value,
+            // phone: document.querySelector('.login-form input[name=phone]').value,
+            // gender: document.querySelector('.login-form input[name=gender]').value,
         })
     })
         .then(_ => document.querySelector('.login-form').reset());
